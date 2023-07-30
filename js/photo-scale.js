409f59c0124868f658d924969be7521545a7da5c
Bogdan Miskichekov <miskichekov@gmail.com>
2023-07-30 21:45:47 +0700

    Добавляет реализацию смены масштаба фотографии

---
+++ js/photo-scale.js
@@ -0,0 +1,25 @@
+const scaleSmallerButton = document.querySelector('.scale__control--smaller');
+const scaleBiggerButton = document.querySelector('.scale__control--bigger');
+const photoScaleValue = document.querySelector('.scale__control--value');
+const photoPreview = document.querySelector('.img-upload__preview img');
+
+const DEFAULT_VALUE = 100;
+const MIN_VALUE = 25;
+
+const getPhotoSmaller = () => {
+  if (parseInt(photoScaleValue.value) > MIN_VALUE) {
+    photoScaleValue.value = parseInt(photoScaleValue.value) - 25 + '%';
+  }
+  photoPreview.style.transform = `scale(${parseInt(photoScaleValue.value) / 100})`
+};
+
+const getPhotoBigger = () => {
+  if (parseInt(photoScaleValue.value) < DEFAULT_VALUE) {
+    photoScaleValue.value = parseInt(photoScaleValue.value) + 25 + '%';
+  }
+  photoPreview.style.transform = `scale(${parseInt(photoScaleValue.value) / 100})`
+};
+
+
+scaleSmallerButton.addEventListener('click', getPhotoSmaller);
+scaleBiggerButton.addEventListener('click', getPhotoBigger);
